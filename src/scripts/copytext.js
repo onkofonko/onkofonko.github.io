6aa19bf0864f55acf69c1160e2ca84ba0b1afa44
onkofonko <36997301+onkofonko@users.noreply.github.com>
2023-06-14 23:28:11 +0200

    Website: Update Discord username

--- src/scripts/copytext.js
+++ src/scripts/copytext.js
@@ -1,17 +1,18 @@
 document.getElementById("clickableDiv").addEventListener("click", function () {
   var copyTextElement = document.getElementById("discord-username");
-
   var originalText = copyTextElement.innerText;
-
   var textToCopy = originalText;
+  var buttonWidth = copyTextElement.offsetWidth;
 
   navigator.clipboard
     .writeText(textToCopy)
     .then(function () {
       copyTextElement.innerText = "Copied!";
+      copyTextElement.style.width = buttonWidth + "px";
 
       setTimeout(function () {
         copyTextElement.innerText = originalText;
+        copyTextElement.style.width = "";
       }, 1000);
     })
     .catch(function (error) {
