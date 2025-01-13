577309064f0c9340c630b092bb6b842e8992241f
onkofonko <ocky852@gmail.com>
2025-01-13 18:41:40 +0100

    Update website

--- src/scripts/copytext.js
+++ src/scripts/copytext.js
@@ -1,21 +1,9 @@
-document.getElementById("clickableDiv").addEventListener("click", function () {
-  var copyTextElement = document.getElementById("discord-username");
-  var originalText = copyTextElement.innerText;
-  var textToCopy = originalText;
-  var buttonWidth = copyTextElement.offsetWidth;
-
-  navigator.clipboard
-    .writeText(textToCopy)
-    .then(function () {
-      copyTextElement.innerText = "Copied!";
-      copyTextElement.style.width = buttonWidth + "px";
-
-      setTimeout(function () {
-        copyTextElement.innerText = originalText;
-        copyTextElement.style.width = "";
-      }, 1000);
-    })
-    .catch(function (error) {
-      console.error("Error copying username to clipboard:", error);
-    });
+document.getElementById("copyDiscord").addEventListener("click", function(event) {
+  event.preventDefault();
+  var username = this.getAttribute("discord-username");
+  navigator.clipboard.writeText(username).then(function() {
+    console.log("Copied to clipboard: " + username);
+  }).catch(function(error) {
+    console.error("Error copying username to clipboard:", error);
+  });
 });
