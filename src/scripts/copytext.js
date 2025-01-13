5e8c984f758284fa09e0b803a8be0f9598070381
onkofonko <ocky852@gmail.com>
2025-01-13 19:22:57 +0100

    Site: Improve performance, responsiveness, and accessibility

--- src/scripts/copytext.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("copyDiscord").addEventListener("click", function(event) {
-  event.preventDefault();
-  var username = this.getAttribute("discord-username");
-  navigator.clipboard.writeText(username).then(function() {
-    console.log("Copied to clipboard: " + username);
-  }).catch(function(error) {
-    console.error("Error copying username to clipboard:", error);
-  });
-});
