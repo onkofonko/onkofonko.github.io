b4bfce93f1522a10873d659e4468c72e4d8aec1f
onkofonko <ocky852@gmail.com>
2025-01-30 18:42:34 +0100

    Site: Update Astro, optimize site, delete Astro partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onkofonko.github.io',
-  integrations: [partytown()]
 });
