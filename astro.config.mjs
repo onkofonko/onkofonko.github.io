582b8cb19ef6e0f417f8b87adaeafc53f420c99b
onkofonko <ocky852@gmail.com>
2025-02-16 19:25:51 +0100

    Site: Implement Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
 import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onkofonko.github.io',
   integrations: [purgecss()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
