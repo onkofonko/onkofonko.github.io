71b4801f6f22d56489ee523257100b63c43f5ba0
onkofonko <ocky852@gmail.com>
2025-05-07 20:28:00 +0200

    Site: Update layout, styles, Astro and other

--- src/scripts/copytext.ts
+++ src/scripts/copytext.ts
@@ -1,26 +1,26 @@
 document
-    .getElementById('copyDiscord')
-    ?.addEventListener('click', async function (event: MouseEvent) {
-        event.preventDefault();
-        const button = this as HTMLElement;
-        const username = button.getAttribute('discord-username');
-        const icon = button.querySelector('svg')?.outerHTML || '';
-        const originalText = button.querySelector('span')?.textContent || 'Discord';
+	.getElementById('copyDiscord')
+	?.addEventListener('click', async function (event: MouseEvent) {
+		event.preventDefault();
+		const button = this as HTMLElement;
+		const username = button.getAttribute('discord-username');
+		const icon = button.querySelector('svg')?.outerHTML || '';
+		const originalText = button.querySelector('span')?.textContent || 'Discord';
 
-        if (username && !button.classList.contains('animating')) {
-            button.classList.add('animating');
+		if (username && !button.classList.contains('animating')) {
+			button.classList.add('animating');
 
-            try {
-                await navigator.clipboard.writeText(username);
-                button.innerHTML = `${icon}<span>Copied!</span>`;
-            } catch (error) {
-                console.error('Error copying username to clipboard:', error);
-                button.innerHTML = `${icon}<span>Failed to copy</span>`;
-            }
+			try {
+				await navigator.clipboard.writeText(username);
+				button.innerHTML = `${icon}<span>Copied!</span>`;
+			} catch (error) {
+				console.error('Error copying username to clipboard:', error);
+				button.innerHTML = `${icon}<span>Failed to copy</span>`;
+			}
 
-            setTimeout(() => {
-                button.innerHTML = `${icon}<span>${originalText}</span>`;
-                button.classList.remove('animating');
-            }, 1500);
-        }
-    });
+			setTimeout(() => {
+				button.innerHTML = `${icon}<span>${originalText}</span>`;
+				button.classList.remove('animating');
+			}, 1500);
+		}
+	});
